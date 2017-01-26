d63a02226d557d6c8d3fe857f9cda448a256ac10
strangecamelcaselogin <strangecamelcaselogin@gmail.com>
2017-01-26 17:44:47 +0300

    more modules

---
+++ functions_.py
@@ -0,0 +1,47 @@
+import cPickle
+import gzip
+
+import numpy as np
+import theano
+import theano.tensor as T
+from theano.tensor import shared_randomstreams
+
+
+# Activation functions for neurons
+def linear(z):
+    return z
+
+
+def ReLU(z):
+    return T.maximum(0.0, z)
+
+
+def dropout_layer(layer, p_dropout):
+    srng = shared_randomstreams.RandomStreams(
+        np.random.RandomState(0).randint(999999))
+    mask = srng.binomial(n=1, p=1 - p_dropout, size=layer.shape)
+    return layer * T.cast(mask, theano.config.floatX)
+
+
+def size(data):
+    """Return the size of the dataset `data`."""
+    return data[0].get_value(borrow=True).shape[0]
+
+
+def load_data_shared(filename="./MNIST/mnist.pkl.gz"):
+    f = gzip.open(filename, 'rb')
+    training_data, validation_data, test_data = cPickle.load(f)
+    f.close()
+
+    def shared(data):
+        """Place the data into shared variables.  This allows Theano to copy
+        the data to the GPU, if one is available.
+
+        """
+        shared_x = theano.shared(
+            np.asarray(data[0], dtype=theano.config.floatX), borrow=True)
+        shared_y = theano.shared(
+            np.asarray(data[1], dtype=theano.config.floatX), borrow=True)
+        return shared_x, T.cast(shared_y, "int32")
+
+    return [shared(training_data), shared(validation_data), shared(test_data)]
--- test.py
+++ test.py
@@ -1,14 +1,33 @@
-import network3
-from network3 import Network
-from network3 import ConvPoolLayer, FullyConnectedLayer, SoftmaxLayer
+# import network3
+from functions_ import load_data_shared
 
-training_data, validation_data, test_data = network3.load_data_shared()
+from network import Network
+from convolution import ConvPoolLayer
+from fullyconnected import FullyConnectedLayer
+from softmax import SoftmaxLayer
+
+training_data, validation_data, test_data = load_data_shared()
 
 mini_batch_size = 10
 
+'''
 net = Network([
     FullyConnectedLayer(n_in=784, n_out=100),
     SoftmaxLayer(n_in=100, n_out=10)], mini_batch_size)
 
 net.SGD(training_data, 60, mini_batch_size, 0.1,
         validation_data, test_data)
+'''
+
+net = Network([
+        ConvPoolLayer(image_shape=(mini_batch_size, 1, 28, 28),
+                      filter_shape=(20, 1, 5, 5),
+                      poolsize=(2, 2)),
+        ConvPoolLayer(image_shape=(mini_batch_size, 20, 12, 12),
+                      filter_shape=(40, 20, 5, 5),
+                      poolsize=(2, 2)),
+        FullyConnectedLayer(n_in=40*4*4, n_out=100),
+        SoftmaxLayer(n_in=100, n_out=10)], mini_batch_size)
+
+net.SGD(training_data, 60, mini_batch_size, 0.1,
+            validation_data, test_data)
